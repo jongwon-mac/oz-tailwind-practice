77534efa7105752fdaf74431bd5c4e832ec00ba3
jongwon-mac <jaketd15@gmail.com>
2025-07-14 02:15:26 -0700

    떼일윈드 쁘렉띠쓰

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
+    <header className="flex justify-between items-center p-2.5 px-5 bg-black">
+      <h2 className="text-white">OZ코딩스쿨</h2>
+      <ul className="flex justify-center items-center gap-5 list-none text-[13px] font-normal text-white">
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
